c579c7b3311be7a44283a60c46e86ec7edd170eb
mhajabin0215 <mhajabin0215@gmail.com>
2026-01-09 19:14:00 +0900

    exit를 입력받기 전까지 계속 입력

--- src/main/groovy/com/java/board/Main.groovy
+++ src/main/groovy/com/java/board/Main.groovy
@@ -7,12 +7,17 @@ public class Main{
         System.out.println("== 자바 텍스트 게시판 ==");
         System.out.println("텍스트 게시판을 시작합니다.");
 
-        System.out.println("명령)");
-        String cmd = sc.nextLine();
+        while (true) {
+            System.out.print("명령)");
+            String cmd = sc.nextLine();
 
-        System.out.printf("입력받은 명령어 : %S\n", cmd);
+            if(cmd.equals("exit")) {
+                System.out.println("텍스트 게시판을 종료합니다.");
+                break;
+            }
 
-        System.out.println("텍스트 게시판을 종료합니다.");
+            System.out.printf("입력받은 명령어 : %S\n", cmd);
+        }
         System.out.println("== 자바 텍스트 게시판 종료 ==");
 
         sc.close();
