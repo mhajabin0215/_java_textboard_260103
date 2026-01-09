0703e57f7da610960b91b2de8fc891b25a1b0433
mhajabin0215 <mhajabin0215@gmail.com>
2026-01-09 19:33:21 +0900

    usr/article/write 명령어 처리

--- src/main/groovy/com/java/board/Main.groovy
+++ src/main/groovy/com/java/board/Main.groovy
@@ -8,15 +8,26 @@ public class Main{
         System.out.println("텍스트 게시판을 시작합니다.");
 
         while (true) {
-            System.out.print("명령)");
+            System.out.print("명령) ");
             String cmd = sc.nextLine();
 
-            if(cmd.equals("exit")) {
+            if(cmd.equals("/usr/article/write")) {
+                System.out.println("== 게시물 작성 ==");
+                System.out.print("제목 : ")
+                String subject = sc.nextLine();
+
+                System.out.print("내용 : ")
+                String content = sc.nextLine();
+                
+                System.out.println("1번 게시물이 등록되었습니다. ")
+            }
+            else if(cmd.equals("exit")) {
                 System.out.println("텍스트 게시판을 종료합니다.");
                 break;
             }
-
-            System.out.printf("입력받은 명령어 : %S\n", cmd);
+            else {
+                System.out.println("잘못 입력 된 명령어입니다.");
+            }
         }
         System.out.println("== 자바 텍스트 게시판 종료 ==");
 
