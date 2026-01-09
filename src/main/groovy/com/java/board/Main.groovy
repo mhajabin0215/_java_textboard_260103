685b2ac97cd34c2da93b4f9f75179b1b438ea232
mhajabin0215 <mhajabin0215@gmail.com>
2026-01-09 19:03:20 +0900

    간단한 명령어 입력 받기

--- src/main/groovy/com/java/board/Main.groovy
+++ src/main/groovy/com/java/board/Main.groovy
@@ -2,10 +2,19 @@ package com.java.board
 
 public class Main{
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("== 자바 텍스트 게시판 ==");
         System.out.println("텍스트 게시판을 시작합니다.");
+
+        System.out.println("명령)");
+        String cmd = sc.nextLine();
+
+        System.out.printf("입력받은 명령어 : %S\n", cmd);
+
         System.out.println("텍스트 게시판을 종료합니다.");
         System.out.println("== 자바 텍스트 게시판 종료 ==");
 
+        sc.close();
     }
 }
