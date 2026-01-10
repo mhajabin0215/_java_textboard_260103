8cccc73bfbd3d98345571f08f3f26487850000d7
mhajabin0215 <mhajabin0215@gmail.com>
2026-01-10 11:51:36 +0900

    게시물 리스트 기능 구현

--- src/main/groovy/com/java/board/Main.groovy
+++ src/main/groovy/com/java/board/Main.groovy
@@ -1,10 +1,15 @@
 package com.java.board
 
-import org.jline.reader.impl.completer.SystemCompleter
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        List<Article> articles = new ArrayList<>();
+
         int lastArticleId = 0;
         Article lastArticle = null;
 
@@ -15,56 +20,82 @@ public class Main{
             System.out.print("명령) ");
             String cmd = sc.nextLine();
 
-            if(cmd.equals("/usr/article/write")) {
+            if (cmd.equals("/usr/article/write")) {
                 System.out.println("== 게시물 작성 ==");
-                System.out.print("제목 : ")
+                System.out.print("제목 : ");
                 String subject = sc.nextLine();
 
-                if(subject.trim().length() == 0){
+                if (subject.trim().isEmpty()) {
                     System.out.println("제목을 입력해주세요.");
                     continue;
                 }
 
-                System.out.print("내용 : ")
+                System.out.print("내용 : ");
                 String content = sc.nextLine();
 
-                if(content.trim().length() == 0){
+                if (content.trim().isEmpty()) {
                     System.out.println("내용을 입력해주세요.");
                     continue;
                 }
 
                 int id = ++lastArticleId;
 
-                //객체 생성 후, 객체가 가지고 있는 변수에 데이터 저장
+                // 객체 생성 후, 객체가 가지고 있는 변수에 데이터 저장
                 Article article = new Article(id, subject, content);
                 lastArticle = article;
 
-                System.out.println("생성 된 게시물 객체 : " + article);
+                articles.add(article);
 
                 System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
-            }
-            else if(cmd.equals("/usr/article/detail")) {
+            } else if (cmd.equals("/usr/article/list")) {
+                if(articles.isEmpty()) {
+                    System.out.println("게시물이 존재하지 않습니다.");
+                    continue;
+                }
+
+                System.out.println("== 게시물 리스트 ==");
+                System.out.println("번호 | 제목");
+
+                // v1
+                /*
+                for(int i = 0; i < articles.size(); i++) {
+                  Article article = articles.get(i);
+
+                  System.out.printf("%d | %s\n", article.id, article.subject);
+                }
+                */
+
+                // v2
+                /*
+                for(Article article : articles) {
+                  System.out.printf("%d | %s\n", article.id, article.subject);
+                }
+                */
+
+                // v3
+                articles.forEach(article
+                        -> System.out.printf("%d | %s\n", article.id, article.subject));
+
+            } else if (cmd.equals("/usr/article/detail")) {
                 Article article = lastArticle;
 
-                if(article == null) {
-                    System.out.println("게시물이 존재하지 않습니다.")
+                if (article == null) {
+                    System.out.println("게시물이 존재하지 않습니다.");
                     continue;
                 }
 
                 System.out.println("== 게시물 상세보기 ==");
-                System.out.printf("번호: %d\n", article.id);
-                System.out.printf("제목: %s\n", article.subject);
-                System.out.printf("내용: %s\n", article.content);
-
-            }
-            else if(cmd.equals("exit")) {
+                System.out.printf("번호 : %d\n", article.id);
+                System.out.printf("제목 : %s\n", article.subject);
+                System.out.printf("내용 : %s\n", article.content);
+            } else if (cmd.equals("exit")) {
                 System.out.println("텍스트 게시판을 종료합니다.");
                 break;
-            }
-            else {
+            } else {
                 System.out.println("잘못 입력 된 명령어입니다.");
             }
         }
+
         System.out.println("== 자바 텍스트 게시판 종료 ==");
 
         sc.close();
@@ -81,6 +112,7 @@ class Article {
         this.subject = subject;
         this.content = content;
     }
+
 
     @Override
     public String toString() {
